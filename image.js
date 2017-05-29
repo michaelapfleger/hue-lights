695088c98db039ff035743aa0dabee15f1ad93fe
Tamara <tamara.czakert@students.fh-hagenberg.at>
2017-05-29 14:05:42 +0200

    main color

--- image.js
+++ image.js
@@ -1,15 +1,12 @@
 var video = document.querySelector("#videoElement");
 
-// check for getUserMedia support
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
 
 if (navigator.getUserMedia) {
-  // get webcam feed if available
   navigator.getUserMedia({video: true}, handleVideo, videoError);
 }
 
 function handleVideo(stream) {
-  // if found attach feed to video element
   video.src = window.URL.createObjectURL(stream);
 }
 
@@ -30,42 +27,20 @@ document.addEventListener('DOMContentLoaded', function(){
 
 },false);
 
+var sourceImage;
+
 function draw(v,c,w,h) {
 
   if(v.paused || v.ended) return false; // if no video, exit here
 
   context.drawImage(v,0,0,w,h); // draw video feed to canvas
-
-  var uri = canvas.toDataURL("image/png"); // convert canvas to data URI
-
-  // console.log(uri); // uncomment line to log URI for testing
-
-  //imgtag.src = uri; // add URI to IMG tag src
 }
 
 document.getElementById('save').addEventListener('click',function(e){
 
   draw(v,context,w,h); // when save button is clicked, draw video feed to canvas
   var colorThief = new ColorThief();
-  console.log('[color]', colorThief.getColor(sourceImage));
+  console.log('[color]', colorThief.getColor(document.querySelector("#canvas")));
 
 });
 
-// for iOS
-
-// create file reader
-var fr;
-
-sel.addEventListener('change',function(e){
-  var f = sel.files[0]; // get selected file (camera capture)
-
-  fr = new FileReader();
-  fr.onload = receivedData; // add onload event
-
-  fr.readAsDataURL(f); // get captured image as data URI
-})
-
-function receivedData() {
-  // readAsDataURL is finished - add URI to IMG tag src
-  imgtag.src = fr.result;
-}
